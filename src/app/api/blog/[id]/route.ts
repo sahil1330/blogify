430654f19b4beed1e5c1b1ced4a03f1bc042d432
sahil1330 <sahilmane025@gmail.com>
2025-01-19 11:14:09 +0530

    Refactor GET route in blog API to simplify parameter handling and remove ObjectId conversion

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,8 +1,9 @@
 import { connectToDatabase } from "@/dbConfig/connectDB";
-import { ObjectId } from "@datastax/astra-db-ts";
+// import { ObjectId } from "@datastax/astra-db-ts";
 import { NextRequest, NextResponse } from "next/server";
+type Params = { id: string }
 
-export async function GET(req: NextRequest, {params}: {params: {id: string}}) {
+export async function GET(req: NextRequest, { params }: { params: Params }) {
   try {
     const { id } = await params;
     // Connect to the database
@@ -10,7 +11,7 @@ export async function GET(req: NextRequest, {params}: {params: {id: string}}) {
 
     // Find the blog with the given ID
     const collection = await db.collection("blogs");
-    const blog = await collection.findOne({ _id: new ObjectId(id) });
+    const blog = await collection.findOne({ _id: id });
     return NextResponse.json({ blog });
   } catch (error) {
     return NextResponse.json(
